29075b25854ce208668a0258e6979a7240495166
jb <jb@DESKTOP-SER8NB8>
2023-01-17 21:16:30 -0500

    Add project files.

---
+++ deliverable1HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace deliverable1HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Solution (1): Hello, World!");
+
+            string stg = "Hello, World! (2)";
+            Console.WriteLine("Solution (2):"+ stg);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Enter a user's name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine(userName);
+
+            Console.WriteLine("Enter the user's age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The user's age is " + age);
+        }
+    }
+}
